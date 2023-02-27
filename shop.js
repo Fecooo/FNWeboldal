6c3e2e10826352f00e6fa73a7ba5a1e7461504db
TothRoland <76938801+TothRoland@users.noreply.github.com>
2023-02-27 21:35:37 +0100

    next shop (CSAK HA 1-KOR VÁLTOZIK!!!)

--- shop.js
+++ shop.js
@@ -1,28 +1,114 @@
 var currentTabok = [];
-var currentLejarat = [99, 99, 24]; // day, hour
+var nextTabok = [];
+var currentLejarat = [99, 99, 24]; // month, day, hour
+var nextLejarat = [99, 99, 24]; // month, day, hour
+var honapok = ["Január", "Február", "Március", "Április", "Május", "Június", "Július", "Augusztus", "Szeptember", "Október", "November", "December"];
 
 window.onload = () => {
     tabokUpdate();
-    timer();
-
     lejar();
+    timer();
 }
 
-/*async function lejar(){
+async function lejar(){
     const now = new Date();
     const currentDayOfMonth = now.getDate();
     const currentHour = now.getHours();
     const currentMinutes = now.getMinutes();
 
-    if(currentHour ) {
-        
+    if(currentLejarat[2] == 1) {
+        if(currentHour = 23 || currentHour == 0){
+
+            let div = document.createElement("div");
+            div.classList.add("container");
+            div.id = "containerNext";
+            body.appendChild(div);
+
+            let h3 = document.createElement("h3");
+            h3.id = "kovetkezo";
+            document.getElementById("containerNext").appendChild(h3);
+
+            let ul = document.createElement("ul");
+            ul.id = "tabokNext";
+            document.getElementById("containerNext").appendChild(ul);
+
+            let p = document.createElement("p");
+            p.id = "ido";
+            document.getElementById("containerNext").appendChild(p);
+
+            const settings  = {
+                method : "GET",
+                headers: {
+                    Authorization : "5bf91731-a9be325a-7d3e0505-a5114310"
+                }
+            }
+
+            let fetchResponse = await fetch("https://fortniteapi.io/v2/shop?lang=en", settings);
+            const data = await fetchResponse.json();
+
+            const tabok = [];
+            for (const key in data.nextRotation) {
+                tabok.push(key);
+
+                console.log(data.nextRotation[key]);
+                if(data.nextRotation[key].split(':')[0].split(' ')[1] < nextLejarat[2]) {
+                    nextLejarat[2] = parseInt(data.nextRotation[key].split(':')[0].split(' ')[1]) + 1;
+                }
+                if(data.nextRotation[key].split(' ')[0].split('-')[2] < nextLejarat[1]) {
+                    nextLejarat[1] = parseInt(data.nextRotation[key].split(' ')[0].split('-')[2]);
+                    if(nextLejarat[2] == 0){
+                        nextLejarat[1]++;
+                    }
+                }
+                if(data.nextRotation[key].split(' ')[0].split('-')[1] < nextLejarat[0]) {
+                    nextLejarat[0] = parseInt(data.nextRotation[key].split(' ')[0].split('-')[1]);
+                }
+            }
+            p.innerText = "A következő shop lejár " + honapok[nextLejarat[0] - 1] + " " + nextLejarat[1] + ". " + nextLejarat[2] + " órakor";
+            h3.innerText = "Következő shop (" + tabok.length + "x):";
+
+            const settings1  = {
+                method : "GET"
+            }
+
+            let fetchResponse1 = await fetch("https://m10q1.wiremockapi.cloud/shoptabok", settings1);
+            const data1 = await fetchResponse1.json();
+
+            for(let i = 0; i < tabok.length; i++) {
+                for (const key in data1) {
+                    if(tabok[i] == key){
+                        nextTabok.push(data1[key]);
+                    }
+                }
+            }
+            
+            const tabStat = {};
+
+            for (let i = 0; i < nextTabok.length; i++) {
+                if (nextTabok[i] in tabStat) {
+                    tabStat[nextTabok[i]] += 1;
+                }
+                else {
+                    tabStat[nextTabok[i]] = 1;
+                }
+            }
+            
+            for (const key in tabStat) {
+                let li = document.createElement("li");
+                li.innerText = key + " (" + tabStat[key] + "x)";
+                document.getElementById("tabokNext").appendChild(li);
+            }
+        }
     }
-}*/
+
+    console.log(currentDayOfMonth + " " + currentHour + " " + currentMinutes);
+}
 
 function update(){
     timer();
     
     tabokUpdate();
+    lejar();
 }
 
 function timer(){
@@ -93,7 +179,6 @@ async function tabokUpdate() {
         }
 
         console.log("A shop lejár: " + currentLejarat[0] + ". napon " + currentLejarat[1] + " órakor");
-        const honapok = ["Január", "Február", "Március", "Április", "Május", "Június", "Július", "Augusztus", "Szeptember", "Október", "November", "December"];
         document.getElementById("lejarat").innerText = "A shop lejár " + honapok[currentLejarat[0] - 1] + " " + currentLejarat[1] + ". " + currentLejarat[2] + " órakor";
         document.getElementById("jelenlegi").innerText = "Jelenlegi shop (" + tabok.length + "x):";
         

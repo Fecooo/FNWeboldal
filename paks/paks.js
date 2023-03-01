ccd3eef8ae0d2490f868537718203421f85e4a04
TothRoland <76938801+TothRoland@users.noreply.github.com>
2023-03-01 23:57:41 +0100

    hatter

--- paks/paks.js
+++ paks/paks.js
@@ -1,4 +1,9 @@
 window.onload = async function(){
+    let response = await fetch("https://cors-anywhere.herokuapp.com/https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/dynamicbackgrounds", {method: "GET"});
+    let data = await response.json();
+    console.log(data.backgrounds.backgrounds[0].backgroundimage)
+    document.getElementById("body").style.backgroundImage = `url(${data.backgrounds.backgrounds[0].backgroundimage})`;
+
     await kiirat();
 }
 

0fb245bbcd03e6b92ba8c0e67c107864585c2419
TothRoland <76938801+TothRoland@users.noreply.github.com>
2023-03-11 23:31:42 +0100

    Add files via upload

--- shop/shop.js
+++ shop/shop.js
@@ -151,7 +151,7 @@ async function tabokUpdate() {
     }
 }
 
-async function lejar(){
+async function lejar() {
     const now = new Date();
     const currentDayOfMonth = parseInt(now.getDate());
     const currentHour = parseInt(now.getHours());
@@ -159,19 +159,20 @@ async function lejar(){
 
     if((currentLejarat[2] == 1 && (currentHour == 23 || currentHour == 0)) || ((currentHour == (currentLejarat[2] - 1)) || (currentHour == (currentLejarat[2] - 2)))){
 
-        let ping =  setInterval(async() => {
+        let pingelo = setInterval(ping(), 15000);
 
+        async function ping() {
             const settings1  = {
                 method : "GET"
             }
-    
+        
             let fetchResponse = await fetch("https://cors-anywhere.herokuapp.com/https://api.nitestats.com/v1/epic/modes-smart", settings1);
             const data = await fetchResponse.json();
-    
+        
             if(data.channels['client-events'].states.length != 1) {
-    
-                clearInterval(ping);
-    
+        
+                clearInterval(pingelo);
+        
                 if(!document.getElementById("containerNext")){
                     let div = document.createElement("div");
                     div.classList.add("container");
@@ -181,40 +182,40 @@ async function lejar(){
                 else{
                     document.getElementById("containerNext").innerHTML = "";
                 }
-    
+        
                 let h3 = document.createElement("h3");
                 h3.id = "kovetkezo";
                 document.getElementById("containerNext").appendChild(h3);
-    
+        
                 let ul = document.createElement("ul");
                 ul.id = "tabokNext";
                 ul.innerHTML = "";
                 document.getElementById("containerNext").appendChild(ul);
-    
+        
                 let p = document.createElement("p");
                 p.id = "ido";
                 document.getElementById("containerNext").appendChild(p);
-    
+        
                 const tabok = [];
                 for (const key in data.channels['client-events'].states[1].state.sectionStoreEnds) {
                     tabok.push(key);
                 }
-    
+        
                 nextLejarat[2] = parseInt(data.channels['client-events'].states[1].state.dailyStoreEnd.split(':')[0].split('T')[1]) + 1;
                 nextLejarat[1] = parseInt(data.channels['client-events'].states[1].state.dailyStoreEnd.split('T')[0].split('-')[2]);
                 if(nextLejarat[2] == 0){
                     nextLejarat[1]++;
                 }
                 nextLejarat[0] = parseInt(data.channels['client-events'].states[1].state.dailyStoreEnd.split('T')[0].split('-')[1]);
-    
+        
                 p.innerText = "A következő shop lejár " + honapok[nextLejarat[0] - 1] + " " + nextLejarat[1] + ". " + nextLejarat[2] + " órakor";
                 h3.innerText = "Következő shop (" + tabok.length + "x):";
-    
+        
                 let fetchResponse1 = await fetch("https://cors-anywhere.herokuapp.com/https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/shop-sections?lang=en", settings1);
                 const data1 = await fetchResponse1.json();
         
                 const display = [];
-    
+        
                 for(let i = 0; i < tabok.length; i++) {
                     for (const key in data1.sectionList.sections) {
         
@@ -256,7 +257,7 @@ async function lejar(){
                 for (let i = 0; i < display.length; i++) {
                     nextTabok.push(display[i]);
                 }
-    
+        
                 for (let i = 0; i < nextTabok.length; i++) {
                     if (nextTabok[i] in tabStatNext) {
                         tabStatNext[nextTabok[i]] += 1;
@@ -271,11 +272,12 @@ async function lejar(){
                     li.innerText = key + " (" + tabStatNext[key] + "x)";
                     document.getElementById("tabokNext").appendChild(li);
                 }
-    
+        
                 kulonbseg();
+        
+                console.log("cs")
             }
-
-        }, 15000);
+        }
         
     }
 }

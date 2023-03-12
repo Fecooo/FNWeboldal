ad40428bbff83df2844ecc49482c60d816db8999
TothRoland <76938801+TothRoland@users.noreply.github.com>
2023-03-12 17:23:56 +0100

    Add files via upload

--- shop/shop.js
+++ shop/shop.js
@@ -8,10 +8,13 @@ var nextLejarat = [99, 99, 99]; // month, day, hour
 var honapok = ["Január", "Február", "Március", "Április", "Május", "Június", "Július", "Augusztus", "Szeptember", "Október", "November", "December"];
 
 window.onload = async function(){
-    let response = await fetch("https://cors-anywhere.herokuapp.com/https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/dynamicbackgrounds", {method: "GET"});
-    let data = await response.json();
-    console.log(data.backgrounds.backgrounds[0].backgroundimage)
-    document.getElementById("body").style.backgroundImage = `url(${data.backgrounds.backgrounds[0].backgroundimage})`;
+    try {
+        let response = await fetch("https://api.allorigins.win/raw?url=https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/dynamicbackgrounds", {method: "GET"});
+        let data = await response.json();
+        document.getElementById("body").style.backgroundImage = `url(${data.backgrounds.backgrounds[0].backgroundimage})`;
+    } catch(err) {
+        console.log(err);
+    }
 
     await tabokUpdate();
     await lejar();
@@ -57,13 +60,9 @@ async function update(){
 }*/
 
 async function tabokUpdate() {
-
-    const settings1  = {
-        method : "GET"
-    }
     
     try {
-        let fetchResponse = await fetch("https://cors-anywhere.herokuapp.com/https://api.nitestats.com/v1/epic/modes-smart", settings1);
+        let fetchResponse = await fetch("https://api.allorigins.win/raw?url=https://api.nitestats.com/v1/epic/modes-smart", { method : "GET" });
         const data = await fetchResponse.json();
 
         const tabok = [];
@@ -83,7 +82,7 @@ async function tabokUpdate() {
         document.getElementById("lejarat").innerText = "A shop lejár " + honapok[currentLejarat[0] - 1] + " " + currentLejarat[1] + ". " + currentLejarat[2] + " órakor";
         document.getElementById("jelenlegi").innerText = "Jelenlegi shop (" + tabok.length + "x):";
         
-        let fetchResponse1 = await fetch("https://cors-anywhere.herokuapp.com/https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/shop-sections?lang=en", settings1);
+        let fetchResponse1 = await fetch("https://api.allorigins.win/raw?url=https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/shop-sections?lang=en", { method : "GET" });
         const data1 = await fetchResponse1.json();
 
         const display = [];
@@ -146,8 +145,8 @@ async function tabokUpdate() {
         }
         
         
-    } catch (e) {
-        console.log(e);
+    } catch(err) {
+        console.log(err);
     }
 }
 
@@ -162,121 +161,125 @@ async function lejar() {
         let pingelo = setInterval(ping(), 15000);
 
         async function ping() {
-            const settings1  = {
-                method : "GET"
-            }
         
-            let fetchResponse = await fetch("https://cors-anywhere.herokuapp.com/https://api.nitestats.com/v1/epic/modes-smart", settings1);
-            const data = await fetchResponse.json();
-        
-            if(data.channels['client-events'].states.length != 1) {
-        
-                clearInterval(pingelo);
-        
-                if(!document.getElementById("containerNext")){
-                    let div = document.createElement("div");
-                    div.classList.add("container");
-                    div.id = "containerNext";
-                    document.getElementById("container-wrapper").appendChild(div);
-                }
-                else{
-                    document.getElementById("containerNext").innerHTML = "";
-                }
-        
-                let h3 = document.createElement("h3");
-                h3.id = "kovetkezo";
-                document.getElementById("containerNext").appendChild(h3);
-        
-                let ul = document.createElement("ul");
-                ul.id = "tabokNext";
-                ul.innerHTML = "";
-                document.getElementById("containerNext").appendChild(ul);
-        
-                let p = document.createElement("p");
-                p.id = "ido";
-                document.getElementById("containerNext").appendChild(p);
-        
-                const tabok = [];
-                for (const key in data.channels['client-events'].states[1].state.sectionStoreEnds) {
-                    tabok.push(key);
-                }
-        
-                nextLejarat[2] = parseInt(data.channels['client-events'].states[1].state.dailyStoreEnd.split(':')[0].split('T')[1]) + 1;
-                nextLejarat[1] = parseInt(data.channels['client-events'].states[1].state.dailyStoreEnd.split('T')[0].split('-')[2]);
-                if(nextLejarat[2] == 0){
-                    nextLejarat[1]++;
-                }
-                nextLejarat[0] = parseInt(data.channels['client-events'].states[1].state.dailyStoreEnd.split('T')[0].split('-')[1]);
-        
-                p.innerText = "A következő shop lejár " + honapok[nextLejarat[0] - 1] + " " + nextLejarat[1] + ". " + nextLejarat[2] + " órakor";
-                h3.innerText = "Következő shop (" + tabok.length + "x):";
-        
-                let fetchResponse1 = await fetch("https://cors-anywhere.herokuapp.com/https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/shop-sections?lang=en", settings1);
-                const data1 = await fetchResponse1.json();
-        
-                const display = [];
-        
-                for(let i = 0; i < tabok.length; i++) {
-                    for (const key in data1.sectionList.sections) {
-        
-                        if(tabok[i] == data1.sectionList.sections[key].sectionId){
-        
-                            if(data1.sectionList.sections[key].sectionDisplayName != null && data1.sectionList.sections[key].sectionDisplayName != ""){
-                                display.push(data1.sectionList.sections[key].sectionDisplayName);
-                            }
-                            else {
-                                let str = data1.sectionList.sections[key].sectionId;
-                                if (str.match(/[0-9AB]$/)) {
-                                    str = str.slice(0, -1);
+            try {
+
+                let fetchResponse = await fetch("https://api.allorigins.win/raw?url=https://api.nitestats.com/v1/epic/modes-smart", { method : "GET" });
+                const data = await fetchResponse.json();
+            
+                if(data.channels['client-events'].states.length != 1) {
+            
+                    clearInterval(pingelo);
+            
+                    if(!document.getElementById("containerNext")){
+                        let div = document.createElement("div");
+                        div.classList.add("container");
+                        div.id = "containerNext";
+                        document.getElementById("container-wrapper").appendChild(div);
+                    }
+                    else{
+                        document.getElementById("containerNext").innerHTML = "";
+                    }
+            
+                    let h3 = document.createElement("h3");
+                    h3.id = "kovetkezo";
+                    document.getElementById("containerNext").appendChild(h3);
+            
+                    let ul = document.createElement("ul");
+                    ul.id = "tabokNext";
+                    ul.innerHTML = "";
+                    document.getElementById("containerNext").appendChild(ul);
+            
+                    let p = document.createElement("p");
+                    p.id = "ido";
+                    document.getElementById("containerNext").appendChild(p);
+            
+                    const tabok = [];
+                    for (const key in data.channels['client-events'].states[1].state.sectionStoreEnds) {
+                        tabok.push(key);
+                    }
+            
+                    nextLejarat[2] = parseInt(data.channels['client-events'].states[1].state.dailyStoreEnd.split(':')[0].split('T')[1]) + 1;
+                    nextLejarat[1] = parseInt(data.channels['client-events'].states[1].state.dailyStoreEnd.split('T')[0].split('-')[2]);
+                    if(nextLejarat[2] == 0){
+                        nextLejarat[1]++;
+                    }
+                    nextLejarat[0] = parseInt(data.channels['client-events'].states[1].state.dailyStoreEnd.split('T')[0].split('-')[1]);
+            
+                    p.innerText = "A következő shop lejár " + honapok[nextLejarat[0] - 1] + " " + nextLejarat[1] + ". " + nextLejarat[2] + " órakor";
+                    h3.innerText = "Következő shop (" + tabok.length + "x):";
+            
+                    let fetchResponse1 = await fetch("https://api.allorigins.win/raw?url=https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/shop-sections?lang=en", settings1);
+                    const data1 = await fetchResponse1.json();
+            
+                    const display = [];
+            
+                    for(let i = 0; i < tabok.length; i++) {
+                        for (const key in data1.sectionList.sections) {
+            
+                            if(tabok[i] == data1.sectionList.sections[key].sectionId){
+            
+                                if(data1.sectionList.sections[key].sectionDisplayName != null && data1.sectionList.sections[key].sectionDisplayName != ""){
+                                    display.push(data1.sectionList.sections[key].sectionDisplayName);
                                 }
-                                if (str.match(/[0-9AB]$/)) {
-                                    str = str.slice(0, -1);
-                                }
-                                if (str.match(/[0-9AB]$/)) {
-                                    str = str.slice(0, -1);
-                                }
-        
-                                for (const key in data1.sectionList.sections) {
-                                    if(data1.sectionList.sections[key].sectionId == str){
-                                        display.push(data1.sectionList.sections[key].sectionDisplayName);
+                                else {
+                                    let str = data1.sectionList.sections[key].sectionId;
+                                    if (str.match(/[0-9AB]$/)) {
+                                        str = str.slice(0, -1);
+                                    }
+                                    if (str.match(/[0-9AB]$/)) {
+                                        str = str.slice(0, -1);
+                                    }
+                                    if (str.match(/[0-9AB]$/)) {
+                                        str = str.slice(0, -1);
+                                    }
+            
+                                    for (const key in data1.sectionList.sections) {
+                                        if(data1.sectionList.sections[key].sectionId == str){
+                                            display.push(data1.sectionList.sections[key].sectionDisplayName);
+                                        }
                                     }
                                 }
                             }
                         }
                     }
-                }
-        
-                lekeres++;
-        
-                if(lekeres != 1) {
-                    lekeres = 1;
-                    nextTabok = [];
-                    tabStatNext = [];
-                }
-        
-                for (let i = 0; i < display.length; i++) {
-                    nextTabok.push(display[i]);
-                }
-        
-                for (let i = 0; i < nextTabok.length; i++) {
-                    if (nextTabok[i] in tabStatNext) {
-                        tabStatNext[nextTabok[i]] += 1;
+            
+                    lekeres++;
+            
+                    if(lekeres != 1) {
+                        lekeres = 1;
+                        nextTabok = [];
+                        tabStatNext = [];
                     }
-                    else {
-                        tabStatNext[nextTabok[i]] = 1;
+            
+                    for (let i = 0; i < display.length; i++) {
+                        nextTabok.push(display[i]);
                     }
+            
+                    for (let i = 0; i < nextTabok.length; i++) {
+                        if (nextTabok[i] in tabStatNext) {
+                            tabStatNext[nextTabok[i]] += 1;
+                        }
+                        else {
+                            tabStatNext[nextTabok[i]] = 1;
+                        }
+                    }
+                    
+                    for (const key in tabStatNext) {
+                        let li = document.createElement("li");
+                        li.innerText = key + " (" + tabStatNext[key] + "x)";
+                        document.getElementById("tabokNext").appendChild(li);
+                    }
+            
+                    kulonbseg();
+            
+                    console.log("cs")
                 }
-                
-                for (const key in tabStatNext) {
-                    let li = document.createElement("li");
-                    li.innerText = key + " (" + tabStatNext[key] + "x)";
-                    document.getElementById("tabokNext").appendChild(li);
-                }
-        
-                kulonbseg();
-        
-                console.log("cs")
+
+            } catch(err) {
+                console.log(err);
             }
+
         }
         
     }

24704c3367af42724ba770b9d453866341e07544
TothRoland <76938801+TothRoland@users.noreply.github.com>
2023-03-12 17:24:35 +0100

    Add files via upload

--- eventflag/flag.js
+++ eventflag/flag.js
@@ -1,28 +1,37 @@
 window.onload = async function(){
-    let response = await fetch("https://cors-anywhere.herokuapp.com/https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/dynamicbackgrounds", {method: "GET"});
-    let data = await response.json();
-    console.log(data.backgrounds.backgrounds[0].backgroundimage)
-    document.getElementById("body").style.backgroundImage = `url(${data.backgrounds.backgrounds[0].backgroundimage})`;
+
+    try{
+        let response = await fetch("https://api.allorigins.win/raw?url=https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/dynamicbackgrounds", {method: "GET"});
+        let data = await response.json();
+        document.getElementById("body").style.backgroundImage = `url(${data.backgrounds.backgrounds[0].backgroundimage})`;
+    } catch(err) {
+        console.log(err);
+    }
 
     await kiirat();
 }
 
 async function kiirat() {
-    let response = await fetch("https://cors-anywhere.herokuapp.com/https://api.nitestats.com/v1/epic/modes-smart", {method: "GET"});
-    const data = await response.json();
+
+    try {
+        let response = await fetch("https://api.allorigins.win/raw?url=https://api.nitestats.com/v1/epic/modes-smart", {method: "GET"});
+        const data = await response.json();
+        
+        let statesLen = data.channels['client-events'].states.length
+        console.log(data.channels['client-events'].states[statesLen-1].state.activeEvents)
     
-    let statesLen = data.channels['client-events'].states.length
-    console.log(data.channels['client-events'].states[statesLen-1].state.activeEvents)
-
-    flags = data.channels['client-events'].states[statesLen-1].state.activeEvents.length
-    nev = []
-    kezdet = []
-    veg = []
-
-    for (let i = 0; i < flags; i++) {
-        nev.push(data.channels['client-events'].states[statesLen-1].state.activeEvents[i].devName)
-        kezdet.push(data.channels['client-events'].states[statesLen-1].state.activeEvents[i].eventStart.replace("T", ". ").replace("Z", " ").replace("-", ".").replace("-", "."))
-        veg.push(data.channels['client-events'].states[statesLen-1].state.activeEvents[i].eventEnd.replace("T", ". ").replace("Z", " ").replace("-", ".").replace("-", "."))
+        flags = data.channels['client-events'].states[statesLen-1].state.activeEvents.length
+        nev = []
+        kezdet = []
+        veg = []
+    
+        for (let i = 0; i < flags; i++) {
+            nev.push(data.channels['client-events'].states[statesLen-1].state.activeEvents[i].devName)
+            kezdet.push(data.channels['client-events'].states[statesLen-1].state.activeEvents[i].eventStart.replace("T", ". ").replace("Z", " ").replace("-", ".").replace("-", "."))
+            veg.push(data.channels['client-events'].states[statesLen-1].state.activeEvents[i].eventEnd.replace("T", ". ").replace("Z", " ").replace("-", ".").replace("-", "."))
+        }
+    } catch(err) {
+        console.log(err);
     }
 
     console.log(nev)
